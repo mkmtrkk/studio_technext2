db3ec666aa76a61b690d02e858fd0c0cf0755592
Rika <mkmtrkk26@gmail.com>
2022-06-14 17:35:49 +0900

    モデルにされた国と場面、なんとか完成しました（T_T）!!!!!!!

--- script.js
+++ script.js
@@ -90,30 +90,30 @@ if(sliderElems && sliderElems.length > 0) {
     //model
     //pic1
     $('.aminme_btn1').on('click',function(){
-        $('.anime1').addClass('hide');
-        $('.anime1').removeClass('hide');
+        $('.picture').addClass('hide');
+        $('.a1').removeClass('hide');
     })
     $('.model_btn1').on('click',function(){
-        $('.model1').addClass('hide');
-        $('.model1').removeClass('hide');
+        $('.picture').addClass('hide');
+        $('.m1').removeClass('hide');
     })
     //pic2
     $('.aminme_btn2').on('click',function(){
-        $('.anime2').addClass('hide');
-        $('.anime2').removeClass('hide');
+        $('.picture').addClass('hide');
+        $('.a2').removeClass('hide');
     })
     $('.model_btn2').on('click',function(){
-        $('.model2').addClass('hide');
-        $('.model2').removeClass('hide');
+        $('.picture').addClass('hide');
+        $('.m2').removeClass('hide');
     })
     //pic3
     $('.aminme_btn3').on('click',function(){
-        $('.anime3').addClass('hide');
-        $('.anime3').removeClass('hide');
+        $('.picture').addClass('hide');
+        $('.a3').removeClass('hide');
     })
     $('.model_btn3').on('click',function(){
-        $('.model3').addClass('hide');
-        $('.model3').removeClass('hide');
+        $('.picture').addClass('hide');
+        $('.m3').removeClass('hide');
     })
     //pic4
     // $('.aminme_btn4').on('click',function(){
